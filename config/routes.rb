becb066a77e813b9772ba7907250584a9fef5c66
Admin <gherbi.yamin.y@gmail.com>
2020-08-14 16:09:44 +0200

    add page books

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages_castor/castor_name'
-  get 'pages_castor/castor'
+  get 'books' => 'books#index'
   # puis plein de commentaires
-end
-
-
-Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
