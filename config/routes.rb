f57f2a13c8509b0840663fbceb852289b188376f
Admin <gherbi.yamin.y@gmail.com>
2020-08-14 17:10:34 +0200

    crud book Ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
   post 'books' => 'books#create'
-  # puis plein de commentaires
+  get 'books/:id' => 'books#show'
+  patch 'books/:id' => 'books#update'
+  delete 'books/:id' => 'books#destroy'
 end
