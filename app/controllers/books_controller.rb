40c9c3d85b5ae7dd7f7acbb376227dee7a7e3f40
Admin <gherbi.yamin.y@gmail.com>
2020-08-14 16:40:30 +0200

    create form add book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,9 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def create
+    Book.create title: params[:title]
+    redirect_to "/books"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index'
+  post 'books' => 'books#create'
   # puis plein de commentaires
 end
