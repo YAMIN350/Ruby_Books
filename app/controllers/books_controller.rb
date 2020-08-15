f38921de03de3512f945c1a17d8e69ad5c927ce4
Admin <gherbi.yamin.y@gmail.com>
2020-08-15 10:03:13 +0200

    add flash message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,18 @@
 class BooksController < ApplicationController
   def index
     @books = Book.page(params[:page]).per(10)
+    @book = Book.new
     @categories = Category.all
   end
 
   def create
     Book.create title: params[:title], category_id: params[:category_id]
+    redirect_to "/books"
+  end
+
+  def destroyAllBook
+    Book.all.destroy_all
+    flash[:destroy_all] = "Tous les livres ont bien été supprimé !"
     redirect_to "/books"
   end
 
@@ -16,6 +23,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update title: params[:title]
+      flash[:success] = 'Le livre a bien été mis à jour !'
       redirect_to "/books/#{params[:id]}"
     else
       render 'show'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
+
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
-  get 'books/:id' => 'books#show'
-  patch 'books/:id' => 'books#update'
-  delete 'books/:id' => 'books#destroy'
+  root 'books#index'
+  resources :books, only: [:index, :create, :show, :update, :destroy]
+  delete 'books/' => 'books#destroyAllBook'
 end
+
